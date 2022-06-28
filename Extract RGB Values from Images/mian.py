db7a2afadc1ecf14cc2dd6f994130c20a529b977
Haowei Cheng <Haowei4671@gmail.com>
2022-06-27 20:21:48 -0700

    colorgram module
    use colorgram to extract color from images

---
+++ Extract RGB Values from Images/mian.py
@@ -0,0 +1,20 @@
+###This code will not work in repl.it as there is no access to the colorgram package here.###
+##We talk about this in the video tutorials##
+import colorgram
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30) # make sure the work directory is match
+# for color in colors:
+#     rgb_colors.append(color.rgb)
+
+# print(rgb_colors) # not the format we want
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb_colors.append((r, g, b))
+
+print(rgb_colors)
